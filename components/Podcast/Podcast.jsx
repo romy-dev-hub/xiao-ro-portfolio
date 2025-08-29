ddd6dc74b3f2d939a07c7745bd782d11209f128b
xiao <roumaissa.hadibi.dev@gmail.com>
2025-08-29 11:36:10 +0100

    fixed background color

--- components/Podcast/Podcast.jsx
+++ components/Podcast/Podcast.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 
 const Podcast = () => {
   return (
-    <section id="podcast" className="py-20 bg-gray-900">
+    <section id="podcast" className="py-20 bg-gray-800">
       <div className="container mx-auto px-4 text-center">
         <motion.div
           initial={{ opacity: 0, y: 20 }}
